2a9c75a01a70971813fc7515a3d55fd40e6cc6c7
s. rannou <mxs@sbrk.org>
2014-12-24 23:04:01 +0100

    Added parsing of a configuration file.

---
+++ src/config.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Config struct {
+	ListenOn string // Interface to listen on
+}
+
+// NewConfig returns a configuration from file pointer by path
+func NewConfig(path string) (*Config, error) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	err = json.Unmarshal(file, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
